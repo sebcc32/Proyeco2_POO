055f36c34d89ee7e23c867793b6c745f8eb5561b
DanDok20 <dhascan2.0@hotmail.com>
2022-05-14 09:55:36 -0500

    Se separaron los imputs de agregar acta en columnas

--- view/EvalTrabajoGrado.py
+++ view/EvalTrabajoGrado.py
@@ -9,16 +9,24 @@ lista_criterios = json.loads(js)
 
 def agregar_acta(st, controller):
     # Objecto que modelará el formulario
+
+    col1, col2, col3 = st.columns(3)
+
     acta_obj = Acta()
-    acta_obj.numero = st.number_input("Id acta")
-    acta_obj.nombre_del_trabajo = st.text_input("Titulo del trabajo")
-    acta_obj.fecha = st.date_input("Fecha de creacion del trabajo")
-    acta_obj.autor = st.text_input("Nombre estudiante")
-    acta_obj.tipo_de_trabajo = st.selectbox('¿Tipo de trabajo?', ["Aplicado", "investigacion"])
-    acta_obj.directora = st.text_input("Nombre Directora")
-    acta_obj.codirector = st.text_input("Nombre Co-director", "N/A")
-    acta_obj.jurado1 = st.text_input("Nombre Primer Jurado")
-    acta_obj.jurado2 = st.text_input("Nombre Segundo Jurado")
+
+    with col1:
+        acta_obj.numero = st.number_input("Id acta")
+        acta_obj.nombre_del_trabajo = st.text_input("Titulo del trabajo")
+        acta_obj.fecha = st.date_input("Fecha de creacion del trabajo")
+    with col2:
+        acta_obj.autor = st.text_input("Nombre estudiante")
+        acta_obj.tipo_de_trabajo = st.selectbox('¿Tipo de trabajo?', ["Aplicado", "investigacion"])
+        acta_obj.directora = st.text_input("Nombre Directora")
+    with col3:
+        acta_obj.codirector = st.text_input("Nombre Co-director", "N/A")
+        acta_obj.jurado1 = st.text_input("Nombre Primer Jurado")
+        acta_obj.jurado2 = st.text_input("Nombre Segundo Jurado")
+
     enviado_btn = st.button("Enviar")
     if enviado_btn:
         controller.agregar_acta(acta_obj)
